d0905fe4ed865379d9a54165072e7534d9431437
Utku Toktar <utkutoktar2016@gmail.com>
2026-08-22 21:02:03 +0300

    fix: make receipt deployment script broadcastable

--- script/DeployAgentTaskReceipt.s.sol
+++ script/DeployAgentTaskReceipt.s.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Script} from "forge-std/Script.sol";
 import {AgentTaskReceipt} from "../src/AgentTaskReceipt.sol";
 
-contract DeployAgentTaskReceipt {
+contract DeployAgentTaskReceipt is Script {
     function run() external returns (AgentTaskReceipt) {
-        return new AgentTaskReceipt();
+        vm.startBroadcast();
+        AgentTaskReceipt receipt = new AgentTaskReceipt();
+        vm.stopBroadcast();
+
+        return receipt;
     }
 }
-
